7f4fa7368c373644cb65dd04df79da80fd13f961
zuoquan <zuoquan@sinosoft.com.cn>
2018-02-06 13:53:53 +0800

    first

---
+++ src/main/java/com/example/demo/BookBean.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("book.properties")
+@ConfigurationProperties(prefix = "book")
+public class BookBean {
+    private String author;
+    private  String  name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+	@Autowired
+	private BookBean bookBean;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+//		SpringApplicationBuilder builder = new SpringApplicationBuilder(DemoApplication.class);
+//		//修改Banner的模式为OFF
+//		builder.bannerMode(Banner.Mode.OFF).run(args);
+	}
+
+
+	@RequestMapping(value = "/book", produces = "text/plain;charset=UTF-8")
+	String index(){
+
+		return "Hello Spring Boot!"+"作者："+bookBean.getAuthor();
+	}
+
+
+}
