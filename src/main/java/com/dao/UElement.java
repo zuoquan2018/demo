2802f2b5e383d1f0ead43b4706c3b1a1884cc4e8
zuoquan <zuoquan@sinosoft.com.cn>
2018-03-30 10:45:29 +0800

    idea commit

---
+++ src/main/java/com/dao/UElement.java
@@ -0,0 +1,18 @@
+package com.dao;
+
+public interface UElement {
+    public class UntilElements {
+        //流程1(进入第一级目标)
+        public void operateI(String uhead, String uleft) {
+        }
+        //流程2(进入第二级目标)
+        public void operateII() {
+        }
+//        //流程3(主要处理类)
+//        public void operateIII(String uhead, String uleft) {
+//        }
+        //报案管理
+        public void report(String uhead, String uleft) {
+        }
+    }
+}
